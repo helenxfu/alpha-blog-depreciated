c8642e8e5a1bdff8b46a4925e6bbef53f01525e1
helenxfu <helenxfu@gmail.com>
2020-01-28 18:01:37 +0900

    code formatted

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,12 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  def new 
+
+  def new
     @article = Article.new
   end
-  def create 
+
+  def create
     # render plain: params[:article].inspect
     debugger
     @article = Article.new(article_params)
@@ -16,36 +18,42 @@ class ArticlesController < ApplicationController
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
-      render 'new'
+      render "new"
     end
   end
+
   def show
     # @article = Article.find(params[:id])
   end
+
   def destroy
     # @article = Article.find(params[:id])
     @article.destroy
     flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
+
   def edit
     # @article = Article.find(params[:id])
   end
+
   def update
     # @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
-      render 'edit'
+      render "edit"
     end
   end
-  private
-    def set_article
-      @article = Article.find(params[:id])
-    end
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
 
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
 
@@ -9,10 +10,18 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to Alpha blog #{@user.username}"
       redirect_to articles_path
     else
-      render 'new'
+      render "new"
     end
   end
+
+  def edit
+  end
+
+  def update
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
