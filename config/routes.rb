602a17df2c72abfb1deca64a87cb0e9b7b0430a0
helenxfu <helenxfu@gmail.com>
2020-01-28 17:22:13 +0900

    messing w stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
   root 'pages#home'
-  get '/about', to: 'pages#about'
+  get 'about', to: 'pages#about'
+  
+  get 'signup', to: 'users#new'
+  
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
