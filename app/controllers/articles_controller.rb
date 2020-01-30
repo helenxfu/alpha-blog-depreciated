d6e93bde93faea765992813a75570cd46fc506b9
helenxfu <helenxfu@gmail.com>
2020-01-30 10:40:24 +0900

    login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
 
   def create
     # render plain: params[:article].inspect
-    debugger
+    # debugger
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # debugger
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "welcome back"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Oops, something wrong with login info"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "See you next time!"
+    redirect_to root_path
+  end
+end
