a93d596818948121c93851015129c530c4ac8463
helenxfu <helenxfu@gmail.com>
2020-03-19 19:17:19 +0900

    lesson

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:edit, :update, :show, :destroy]
 
   def index
     # @categories = Category.all
@@ -20,10 +21,37 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    # @category = Category.find(params[:id])
+  end
+
+  def update
+    # @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name successfully changed"
+      redirect_to category_path(@category)
+    else
+      render "edit"
+    end
+  end
+
   def show
+    # @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def destroy
+    # @category = Category.find(params[:id])
+    @category.destroy
+    flash[:danger] = "Category deleted"
+    redirect_to categories_path
   end
 
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
@@ -33,9 +61,6 @@ class CategoriesController < ApplicationController
     if !logged_in? || (logged_in? and !current_user.admin?)
       flash[:danger] = "admin only!"
       redirect_to categories_path
-    else
-      @category = Category.new
-      render "new"
     end
   end
 end
